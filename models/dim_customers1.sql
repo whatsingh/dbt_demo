f985eb5039891047798c9f249b5a38860494a172
NEWTON9@live.com <NEWTON9@live.com>
2023-02-13 01:40:27 +0000

    created stg and ref

--- models/dim_customers1.sql
+++ models/dim_customers1.sql
@@ -3,14 +3,29 @@
     materialized='table'
   )
 }}
-with customers as
-( select * from {{
-    ref('stg_customers')
-}}),
-with orders as
-( select * from {{
-    ref('stg_orders')
-}}),
+
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from `dbt-tutorial`.jaffle_shop.customers
+
+),
+
+orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from `dbt-tutorial`.jaffle_shop.orders
+
+),
 
 customer_orders as (
 
