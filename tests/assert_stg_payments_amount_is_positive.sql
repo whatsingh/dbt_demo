0529464e275253b6d63f8e75432b30c02a2bd5fd
NEWTON9@live.com <NEWTON9@live.com>
2023-02-13 22:21:51 +0000

    updated stripe test

---
+++ tests/assert_stg_payments_amount_is_positive.sql
@@ -0,0 +1,10 @@
+with
+payments as (
+    select * from {{ref('stg_payment')}}
+)
+select 
+    orderid,
+    sum(amount) total_amount
+    from payments
+    group by orderid
+    having total_amount < 0
