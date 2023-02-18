a24652ecb9d60985dbae058967fa34063f4c5027
NEWTON9@live.com <NEWTON9@live.com>
2023-02-18 04:05:09 +0000

    update

--- models/disease/staging_d/stg_disease_total.sql
+++ models/disease/staging_d/stg_disease_total.sql
@@ -3,6 +3,7 @@
 
 WITH TOTAL_DISEASE AS 
 (SELECT STATE,disease,sum(cast (cases as numeric) ) total_cases FROM `bigquery-public-data.usa_contagious_disease.project_tycho_reports` 
+where cases <> '\\N'
 GROUP BY STATE,disease),
 TOTAL_PER_STATE  AS
 (SELECT STATE, SUM(TOTAL_CASES) TOTAL_FOR_STATE FROM TOTAL_DISEASE
