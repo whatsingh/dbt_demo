6a55cc37ba94383d9965caf4f5d0683f2480bafd
NEWTON9@live.com <NEWTON9@live.com>
2023-02-19 17:54:21 +0000

    bike ride

---
+++ models/BikeRideShare/bike_staging/stg_station.sql
@@ -0,0 +1,9 @@
+with
+    station as (
+        select *
+        from `bigquery-public-data.austin_bikeshare.bikeshare_stations`
+        where floor(station_id / 100) = 25
+        limit 100
+    )
+select *
+from station
---
+++ models/BikeRideShare/bike_staging/stg_total_time.sql
@@ -0,0 +1,3 @@
+select station_id, sum(duration_minutes) total_minutes
+from {{ ref("stg_rider") }}
+group by station_id
