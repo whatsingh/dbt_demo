7fd88350a94b9afff8c6b358983e5d12258c0923
NEWTON9@live.com <NEWTON9@live.com>
2023-02-20 01:09:29 +0000

    bike ride

--- models/bike_staging/bike_rides/stg_rider.sql
+++ models/bike_staging/bike_rides/stg_rider.sql
@@ -13,7 +13,6 @@ with
             duration_minutes,
             start_station_id station_id
         from `bigquery-public-data.austin_bikeshare.bikeshare_trips`
-       -- from {{source('austin_bikeshare','bikeshare_trips')}}
         where subscriber_type like '%Pass%'
         limit 100
     )
