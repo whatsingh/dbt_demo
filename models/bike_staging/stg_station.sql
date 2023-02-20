c7d5358c46c1a9fb7b92b21da6566c522214d32b
NEWTON9@live.com <NEWTON9@live.com>
2023-02-20 00:53:50 +0000

    bike ride

--- models/bike_staging/stg_station.sql
+++ models/bike_staging/stg_station.sql
@@ -2,7 +2,6 @@ with
     station as (
         select *
         from `bigquery-public-data.austin_bikeshare.bikeshare_stations`
-        --from {{source('austin_bikeshare','bikeshare_stations')}}
         where floor(station_id / 100) = 25
         limit 100
     )
