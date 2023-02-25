5d129c416af4e3ae21bf621e876d5549dfc26fef
NEWTON9@live.com <NEWTON9@live.com>
2023-02-25 03:22:13 +0000

    invocation_id

--- models/incremental/stg_incremental.sql
+++ models/incremental/stg_incremental.sql
@@ -1,3 +1,3 @@
-{{config(materialized = 'incremental')}}
-SELECT geo_id,median_rent FROM `bigquery-public-data.census_bureau_acs.blockgroup_2010_5yr` 
+{{config(materialized = 'table')}}
+SELECT geo_id,median_rent, {{invocation_id}} as invocation_id FROM `bigquery-public-data.census_bureau_acs.blockgroup_2010_5yr` 
 where median_rent = 915.0 
