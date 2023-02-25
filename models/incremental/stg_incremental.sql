7aa3198ee5c5b7453d8f45092c1c6ad9cf176b6d
NEWTON9@live.com <NEWTON9@live.com>
2023-02-25 02:50:24 +0000

    incremental

--- models/incremental/stg_incremental.sql
+++ models/incremental/stg_incremental.sql
@@ -1,3 +1,3 @@
 {{config(materialized = 'incremental')}}
 SELECT geo_id,median_rent FROM `bigquery-public-data.census_bureau_acs.blockgroup_2010_5yr` 
-where median_rent = 920.0 
+where median_rent = 915.0 
